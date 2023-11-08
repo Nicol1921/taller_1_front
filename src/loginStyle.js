ac03c29eb6ee5ade4b25d9c80ebcf8e910ff8705
Nicol1921 <128154864+Nicol1921@users.noreply.github.com>
2023-11-08 01:30:24 -1200

    Update loginStyle.js

--- src/loginStyle.js
+++ src/loginStyle.js
@@ -2,24 +2,24 @@ import { css } from "lit-element";
 
 export default css`
   #form {
-    background: #8a98a6; /* Change background color to a different shade of gray */
+    background: #8a98a6; 
     padding: 98px;
     border-radius: 20px;
     border: 2px solid grey;
   }
 
   #form2 {
-    background: #484f57; /* Change background color to a darker shade of gray */
+    background: #484f57; 
     padding: 98px;
     border-radius: 20px;
     right: 100px;
-    border: 2px solid #c72020; /* Change border color to a specific shade of red */
+    border: 2px solid #c72020; 
   }
   #container-username {
-    background-color: #ffffff; /* Change background color to white */
+    background-color: #ffffff;
   }
   #container-password {
-    background-color: #ffffff; /* Change background color to white */
+    background-color: #ffffff;
   }
 `;
 
